9f143e6334f2af45c962f988659f88e7b6bbbd16
jmjusmin <jmjusmin1412@gmail.com>
2022-10-01 23:03:24 -0700

    Init project

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//declare variable
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
